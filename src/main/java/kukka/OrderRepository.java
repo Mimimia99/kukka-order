1210be3c19111e19d039554c13769da235342918
Mimimia99 <88864740+Mimimia99@users.noreply.github.com>
2021-10-01 00:39:20 +0900

    Update OrderRepository.java

--- src/main/java/kukka/OrderRepository.java
+++ src/main/java/kukka/OrderRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
-//@RepositoryRestResource(collectionResourceRel="orders", path="orders")
+@RepositoryRestResource(collectionResourceRel="orders", path="orders")
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
 
 }
